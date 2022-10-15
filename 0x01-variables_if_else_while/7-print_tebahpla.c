c239d64616bd7d00d3c0e1b904e5c4b3d9dceb84
Samuel Ojo <samdamz18@gmail.com>
2022-10-15 05:48:00 +0100

    Another commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - reverse alphabets
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
